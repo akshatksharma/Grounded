8d7708d963744744c932bb0dede72a0b94130d4d
Akshat Sharma <akshatsharma@MacBook-Pro.local>
2020-06-29 13:03:59 -0500

    Fixed centering issue and cleaned up some styles

--- src/About/About.js
+++ src/About/About.js
@@ -1,59 +1,68 @@
 import React from "react";
-import Navbar from "./Navbar/Navbar.js";
 import "./About.css";
 
 const About = () => {
   let content = (
-    <Navbar />
-      <div className="page page--intro flow">
-        <div className="row">
-          <div className="column">
-            <h1 className="title">About Our Project</h1>
-          </div>
-        </div>
-        <div className="row">
-          <div className="column">
+    <React.Fragment>
+      <div className="page page--about flow">
+        <h1 className="subtitle subtitle--about bold text--center">
+          About Our Project
+        </h1>
+        <div className="about__content">
+          <div className="contactinfo column">
             <div className="box_outside">
-              <div className="box_content">
-                <div className="box_title">
-                  Contact Info:
-             </div>
-                <div className="box_sub_title">
-                  Email:
-             </div>
-                <div className="text">
-                  groundedarchives@gmail.com
-             </div>
-                <div className="box_sub_title">
-                  Phone Number:
-             </div>
-                <div className="text">
-                  (314) 312-3524
-             </div>
+              <div className="box_content flow">
+                <div className="box_title">Contact Info:</div>
+                <div className="contactinfo__email">
+                  <div className="box_sub_title">Email:</div>
+                  <a
+                    href="mailto:groundedarchives@gmail.com?subject=Question"
+                    target="_blank"
+                  >
+                    groundedarchives@gmail.com
+                  </a>
+                </div>
+                <div className="contactInfo__number">
+                  <div className="box_sub_title">Phone Number:</div>
+                  <a classname="text" href="tel:314-312-3524">
+                    (314) 312-3524
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-          <div className="column">
-            <p className="text about_content">
-              The enduring stories of global events are often found in the objects that are left behind: artifacts
-                in museums, keepsakes passed down through families, houses that withstood war, famine, or plague.</p><br></br>
-            <p className="text about_content">Living in quarantine has forced many of us to find new meaning in the spaces we inhabit and the objects that surround us.</p><br></br>
-            <p className="text about_content">“Grounded: The Pandemic Archive” is our effort to assemble a living archive of the objects that tell our stories.
-            Through this project, we invite you to find and preserve an object that holds a piece of your story,
-                and add your voice to history.</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="column">
-            <div className="example_back">
-              <h1 className="example_title">Example Submissions</h1>
-            </div>
+          <div className="abouttext column flow">
+            <p className="text--intro about_content">
+              The enduring stories of global events are often found in the
+              objects that are left behind: artifacts in museums, keepsakes
+              passed down through families, houses that withstood war, famine,
+              or plague.
+            </p>
+            <p className="text--intro about_content">
+              Living in quarantine has forced many of us to find new meaning in
+              the spaces we inhabit and the objects that surround us.
+            </p>
+            <p className="text--intro about_content">
+              “Grounded: The Pandemic Archive” is our effort to assemble a
+              living archive of the objects that tell our stories. Through this
+              project, we invite you to find and preserve an object that holds a
+              piece of your story, and add your voice to history.
+            </p>
           </div>
         </div>
       </div>
-    );
+      <div className="page page--about page--about--example flow">
+        <h1 className="subtitle subtitle--about bold text--center">
+          Example Submissions
+        </h1>
+        <div className="column">
+          <div className="example_back"></div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 
-    return content;
-  };
+  return content;
+};
 
-  export default About;
+export default React.memo(About);
